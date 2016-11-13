3f82a204bd983524bd6703617b6c3f48035564ec
Hisai Toru <toru@torus.jp>
2016-11-13 21:40:06 +0100

    game example

---
+++ game/pilot.scm
@@ -0,0 +1,114 @@
+(load "../constraint")
+
+(define (skip-if con)
+  (when con (raise (condition (<mecs-skip-calculation>)))))
+
+(define home (mecs-new-var))
+(define ui-home->smith (mecs-new-var))
+(define ui-smith->home (mecs-new-var))
+(define smith (mecs-new-var))
+(define ui-smith->hill (mecs-new-var))
+(define ui-hill->smith (mecs-new-var))
+(define hill (mecs-new-var))
+(define has-stone? (mecs-new-var))
+(define ui-pick-stone (mecs-new-var))
+(define ui-pick-knife (mecs-new-var))
+(define has-knife? (mecs-new-var))
+(define ui-smith->hilou (mecs-new-var))
+(define hilou (mecs-new-var))
+
+(define show-home
+  (mecs-new-func (lambda (home)    ; -> ()
+                   (skip-if (not home))
+                   (print "わたしの家")
+                   (print "お母さん「鍛冶屋さんで包丁を作ってもらって、」")
+                   (print "お母さん「ヒロウ村のおじいちゃんに持って行ってちょうだい」")
+                   (values))))
+(mecs-connect! show-home `(,home) ())
+
+(define home->smith
+ (mecs-new-func (lambda (ui-home->smith) ; -> home smith
+                  (skip-if (not ui-home->smith))
+                  (print "鍛冶屋さんへ移動")
+                  (values #f #t))))
+(define smith->home
+ (mecs-new-func (lambda (ui-smith->home) ; -> home smith
+                  (skip-if (not ui-smith->home))
+                  (print "わたしの家へ移動")
+                  (values #t #f))))
+
+(define show-smith
+  (mecs-new-func (lambda (smith has-stone?)    ; -> ()
+                   (skip-if (not smith))
+                   (print "鍛冶屋")
+                   (if has-stone?
+                       (begin
+                         (print "鍛冶屋「これは良い石だ。見つけてきてくれてありがとう」")
+                         (print "鍛冶屋「この包丁をおじいさんに持って行きなさい」"))
+                       (begin
+                         (print "わたし「鍛冶屋さんこんにちは」")
+                         (print "わたし「ヒロウ村のおじいさんの為に包丁をください」")
+                         (print "鍛冶屋「あいにく石を切らしてしまって包丁が作れないんだよ」")
+                         (print "鍛冶屋「山へ行って石を拾ってきてくれるかい？」")
+                         )
+                       )
+                   (values))))
+(mecs-connect! show-smith `(,smith ,has-stone?) ())
+
+(define pick-knife
+ (mecs-new-func (lambda (ui-pick-knife) ; -> has-knife?
+                  (skip-if (not ui-pick-knife))
+                  (print "包丁を手に入れた")
+                  #t)))
+(mecs-connect! pick-knife `(,ui-pick-knife) `(,has-knife?))
+
+(mecs-connect! home->smith `(,ui-home->smith) `(,home ,smith))
+(mecs-connect! smith->home `(,ui-smith->home) `(,home ,smith))
+
+(define show-hill
+  (mecs-new-func (lambda (home)    ; -> ()
+                   (skip-if (not home))
+                   (print "山")
+                   (print "わたし「あそこに光る石がある！」")
+                   (print "わたし「あれを鍛冶屋さんに持って行けばいいのね」")
+                   (values))))
+(mecs-connect! show-hill `(,hill) ())
+
+(define smith->hill
+ (mecs-new-func (lambda (ui-smith->hill) ; -> smith hill
+                  (skip-if (not ui-smith->hill))
+                  (print "山へ移動")
+                  (values #f #t))))
+(define hill->smith
+ (mecs-new-func (lambda (ui-hill->smith) ; -> smith hill
+                  (skip-if (not ui-hill->smith))
+                  (print "鍛冶屋へ移動")
+                  (values #t #f))))
+(mecs-connect! smith->hill `(,ui-smith->hill) `(,smith ,hill))
+(mecs-connect! hill->smith `(,ui-hill->smith) `(,smith ,hill))
+
+(define pick-stone
+ (mecs-new-func (lambda (ui-pick-stone) ; -> has-stone?
+                  (skip-if (not ui-pick-stone))
+                  (print "光る石を拾った")
+                  #t)))
+
+(mecs-connect! pick-stone `(,ui-pick-stone) `(,has-stone?))
+
+;; ヒロウ村へ
+;; 一方通行
+(define smith->hilou
+ (mecs-new-func (lambda (ui-smith->hilou) ; -> smith hilou
+                  (skip-if (not ui-smith->hilou))
+                  (print "ヒロウ村へ移動")
+                  (values #f #t))))
+(mecs-connect! smith->hilou `(,ui-smith->hilou) `(,smith ,hilou))
+
+(define show-hilou
+  (mecs-new-func (lambda (hilou)    ; -> ()
+                   (skip-if (not hilou))
+                   (print "ヒロウ村")
+                   (print "わたし「おじちゃんはどこかな？」")
+                   (print "村人「おじいちゃんは港の近くの家にいるよ」")
+                   (values))))
+(mecs-connect! show-hilou `(,hilou) ())
---
+++ game/test.scm
@@ -0,0 +1,24 @@
+(use gauche.test)
+
+(test-start "名称未定。パイロット版")
+(load "./pilot.scm")
+
+(test-section "初めの町")
+
+(mecs-update! `((,home . #t)))
+
+(mecs-update! `((,ui-home->smith . #t)))
+
+(mecs-update! `((,has-stone? . #f)))
+
+(mecs-update! `((,ui-smith->hill . #t)))
+
+(mecs-update! `((,ui-pick-stone . #t)))
+
+(mecs-update! `((,ui-hill->smith . #t)))
+
+(mecs-update! `((,ui-pick-knife . #t)))
+
+(mecs-update! `((,ui-smith->hilou . #t)))
+
+(test-end)

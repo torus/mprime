211ae2b1607707ba97d99219e58630465fc47446
Hisai Toru <toru@torus.jp>
2016-11-20 14:57:18 +0100

    refactoring...

--- game/pilot.scm
+++ game/pilot.scm
@@ -4,17 +4,19 @@
   (when con (raise (condition (<mecs-skip-calculation>)))))
 
 (define loc:home (mecs-new-var))
-(define ui:home->smith (mecs-new-var))
-(define ui:smith->home (mecs-new-var))
-(define loc:smith (mecs-new-var))
-(define ui:smith->hill (mecs-new-var))
-(define ui:hill->smith (mecs-new-var))
+(define ui:home->smith-1 (mecs-new-var))
+(define ui:smith-1->home (mecs-new-var))
+(define loc:smith-1 (mecs-new-var))
+(define loc:smith-2 (mecs-new-var))
+(define loc:smith-3 (mecs-new-var))
+(define ui:smith-1->hill (mecs-new-var))
+(define ui:hill->smith-2 (mecs-new-var))
 (define loc:hill (mecs-new-var))
 (define has-stone? (mecs-new-var))
 (define ui:pick-stone (mecs-new-var))
 (define ui:pick-knife (mecs-new-var))
 (define has-knife? (mecs-new-var))
-(define ui:smith->hilou (mecs-new-var))
+(define ui:smith-3->hilou (mecs-new-var))
 (define loc:hilou (mecs-new-var))
 
 (define (add-location! name proc loc . inputs)
@@ -56,43 +58,43 @@
    )
  loc:home)
 
-(add-trigger! 'ui:home->smith "鍛冶屋さんへ" loc:home)
+(add-trigger! 'ui:home->smith-1 "鍛冶屋さんへ" loc:home)
 
-(add-path! ui:home->smith loc:home loc:smith)
-(add-path! ui:smith->home loc:smith loc:home)
+(add-path! ui:home->smith-1 loc:home loc:smith-1)
+(add-path! ui:smith-1->home loc:smith-1 loc:home)
 
 (add-location!
- "鍛冶屋"
- (lambda (has-stone? has-knife?)
-   (if has-stone?
-       (begin
-         (print "鍛冶屋シナガー「これは良い石だ。見つけてきてくれてありがとう」")
-         (print "鍛冶屋シナガー「この包丁をおじいさんに持って行きなさい」")
-         ;; (show-trigger 'ui:pick-knife "包丁を受け取る")
-         ;; (show-trigger 'ui:smith->hilou "ヒロウ村へ"))
-         )
-       (if has-knife?
-           (begin
-             (print "鍛冶屋シナガー「ヒロウ村は坂を降りたところだよ」"))
-           (begin
-             (print "わたし「鍛冶屋さんこんにちは」")
-             (print "わたし「ヒロウ村のおじいさんの為に包丁をください」")
-             (print "鍛冶屋シナガー「あいにく砥石がなくて包丁が作れないんだよ」")
-             (print "鍛冶屋シナガー「オシャーゲ山へ行って光るアカッカ石を拾ってきてくれるかい？」")
-             (show-trigger 'ui:smith->hill "オシャーゲ山へ")
-             )
-           )))
- loc:smith has-stone? has-knife?)
+ "鍛冶屋 1"
+ (lambda ()
+   (print "わたし「鍛冶屋さんこんにちは」")
+   (print "わたし「ヒロウ村のおじいさんの為に包丁をください」")
+   (print "鍛冶屋シナガー「あいにく砥石がなくて包丁が作れないんだよ」")
+   (print "鍛冶屋シナガー「オシャーゲ山へ行って光るアカッカ石を拾ってきてくれるかい？」")
+   (show-trigger 'ui:smith-1->hill "オシャーゲ山へ"))
+ loc:smith-1)
 
-(add-trigger! 'ui:pick-knife "包丁を受け取る" loc:smith has-stone?)
-(add-trigger! 'ui:smith->hilou "ヒロウ村へ" loc:smith has-knife?)
+(add-location!
+ "鍛冶屋 2"
+ (lambda ()
+   (print "鍛冶屋シナガー「これは良い石だ。見つけてきてくれてありがとう」")
+   (print "鍛冶屋シナガー「この包丁をおじいさんに持って行きなさい」")
+   (show-trigger 'ui:pick-knife "包丁を受け取る"))
+ loc:smith-2)
 
-(define pick-knife
- (mecs-new-func (lambda (ui:pick-knife) ; -> has-knife?
+(add-location!
+ "鍛冶屋 3"
+ (lambda ()
+   (print "鍛冶屋シナガー「ヒロウ村は坂を降りたところだよ」")
+   (show-trigger 'ui:smith-3->hilou "ヒロウ村へ"))
+ loc:smith-3)
+
+#;(define pick-knife
+ (mecs-new-func (lambda (ui:pick-knife) ; -> loc:smith-3
                   (skip-if (not ui:pick-knife))
                   (print "シナガーの包丁を手に入れた")
-                  (values #t #f))))
-(mecs-connect! pick-knife `(,ui:pick-knife) `(,has-knife? ,has-stone?))
+                  #t)))
+
+(add-path! ui:pick-knife loc:smith-2 loc:smith-3)
 
 (add-location!
  "山"
@@ -103,20 +105,20 @@
    )
  loc:hill)
 
-(add-path! ui:smith->hill loc:smith loc:hill)
-(add-path! ui:hill->smith loc:hill loc:smith)
+(add-path! ui:smith-1->hill loc:smith-1 loc:hill)
+(add-path! ui:hill->smith-2 loc:hill loc:smith-2)
 
-(define pick-stone
+#;(define pick-stone
  (mecs-new-func (lambda (ui:pick-stone) ; -> has-stone?
                   (skip-if (not ui:pick-stone))
                   (print "光る石を拾った")
                   #t)))
 
-(mecs-connect! pick-stone `(,ui:pick-stone) `(,has-stone?))
+#;(mecs-connect! pick-stone `(,ui:pick-stone) `(,has-stone?))
 
 ;; ヒロウ村へ
 ;; 一方通行
-(add-path! ui:smith->hilou loc:smith loc:hilou)
+(add-path! ui:smith-3->hilou loc:smith-3 loc:hilou)
 
 (add-location!
  "ヒロウ村"
--- game/test.scm
+++ game/test.scm
@@ -7,18 +7,18 @@
 
 (mecs-update! `((,loc:home . #t)))
 
-(mecs-update! `((,ui:home->smith . #t)))
+(mecs-update! `((,ui:home->smith-1 . #t)))
 
-(mecs-update! `((,has-knife? . #f) (,has-stone? . #f)))
+;(mecs-update! `((,has-knife? . #f) (,has-stone? . #f)))
 
-(mecs-update! `((,ui:smith->hill . #t)))
+(mecs-update! `((,ui:smith-1->hill . #t)))
 
 (mecs-update! `((,ui:pick-stone . #t)))
 
-(mecs-update! `((,ui:hill->smith . #t)))
+(mecs-update! `((,ui:hill->smith-2 . #t)))
 
 (mecs-update! `((,ui:pick-knife . #t)))
 
-(mecs-update! `((,ui:smith->hilou . #t)))
+(mecs-update! `((,ui:smith-3->hilou . #t)))
 
 (test-end)

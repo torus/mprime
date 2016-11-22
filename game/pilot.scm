c59ee5575612066526b8aab8c32907091d35206b
Hisai Toru <toru@torus.jp>
2016-11-22 19:27:46 +0100

    refactoring...

--- game/pilot.scm
+++ game/pilot.scm
@@ -1,25 +1,29 @@
 (load "../constraint")
 
+
+;; fourteen
+
 (define (skip-if con)
   (when con (raise (condition (<mecs-skip-calculation>)))))
 
-(define loc:home (mecs-new-var))
+(define scn:home (mecs-new-var))
 (define ui:home->smith-1 (mecs-new-var))
 (define ui:smith-1->home (mecs-new-var))
-(define loc:smith-1 (mecs-new-var))
-(define loc:smith-2 (mecs-new-var))
-(define loc:smith-3 (mecs-new-var))
-(define ui:smith-1->hill (mecs-new-var))
-(define ui:hill->smith-2 (mecs-new-var))
-(define loc:hill (mecs-new-var))
+(define scn:smith-1 (mecs-new-var))
+(define scn:smith-2 (mecs-new-var))
+(define scn:smith-3 (mecs-new-var))
+(define ui:smith-1->hill-1 (mecs-new-var))
+(define ui:hill-2->smith-2 (mecs-new-var))
+(define scn:hill-1 (mecs-new-var))
+(define scn:hill-2 (mecs-new-var))
 (define has-stone? (mecs-new-var))
 (define ui:pick-stone (mecs-new-var))
 (define ui:pick-knife (mecs-new-var))
 (define has-knife? (mecs-new-var))
 (define ui:smith-3->hilou (mecs-new-var))
-(define loc:hilou (mecs-new-var))
+(define scn:hilou (mecs-new-var))
 
-(define (add-location! name proc loc . inputs)
+(define (ft-add-scene! name proc loc . inputs)
   (mecs-connect!
    (mecs-new-func
     (lambda (loc . rest)    ; -> ()
@@ -29,7 +33,7 @@
       (values)))
    (cons loc inputs) ()))
 
-(define (add-path! trigger from to)
+(define (ft-add-path! trigger from to)
   (mecs-connect!
    (mecs-new-func (lambda (trigger)
                     (skip-if (not trigger))
@@ -37,93 +41,77 @@
                     (values #f #t)))
    `(,trigger) `(,from ,to)))
 
-(define (show-trigger trigger-name destination)
+(define (ft-show-trigger trigger-name destination)
   (print #`"* ,|destination| ,`(mecs-update! `((,,trigger-name . #t)))")
   )
 
-(define (add-trigger! trigger-name text . flags)
-  (mecs-connect!
-   (mecs-new-func (lambda triggers
-                    (skip-if (any not triggers))
-                    (show-trigger trigger-name text)
-                    ))
-   flags ())
-  )
-
-(add-location!
+(ft-add-scene!
  "わたしの家"
  (lambda ()
    (print "お母さん「鍛冶屋のシナガーさんに包丁を作ってもらって、」")
    (print "お母さん「ヒロウ村のおじいちゃんに持って行ってちょうだい」")
+   (ft-show-trigger 'ui:home->smith-1 "鍛冶屋さんへ")
    )
- loc:home)
+ scn:home)
 
-(add-trigger! 'ui:home->smith-1 "鍛冶屋さんへ" loc:home)
+(ft-add-path! ui:home->smith-1 scn:home scn:smith-1)
+(ft-add-path! ui:smith-1->home scn:smith-1 scn:home)
 
-(add-path! ui:home->smith-1 loc:home loc:smith-1)
-(add-path! ui:smith-1->home loc:smith-1 loc:home)
-
-(add-location!
+(ft-add-scene!
  "鍛冶屋 1"
  (lambda ()
    (print "わたし「鍛冶屋さんこんにちは」")
    (print "わたし「ヒロウ村のおじいさんの為に包丁をください」")
    (print "鍛冶屋シナガー「あいにく砥石がなくて包丁が作れないんだよ」")
    (print "鍛冶屋シナガー「オシャーゲ山へ行って光るアカッカ石を拾ってきてくれるかい？」")
-   (show-trigger 'ui:smith-1->hill "オシャーゲ山へ"))
- loc:smith-1)
+   (ft-show-trigger 'ui:smith-1->hill-1 "オシャーゲ山へ"))
+ scn:smith-1)
 
-(add-location!
+(ft-add-scene!
  "鍛冶屋 2"
  (lambda ()
    (print "鍛冶屋シナガー「これは良い石だ。見つけてきてくれてありがとう」")
    (print "鍛冶屋シナガー「この包丁をおじいさんに持って行きなさい」")
-   (show-trigger 'ui:pick-knife "包丁を受け取る"))
- loc:smith-2)
+   (ft-show-trigger 'ui:pick-knife "包丁を受け取る"))
+ scn:smith-2)
 
-(add-location!
+(ft-add-scene!
  "鍛冶屋 3"
  (lambda ()
    (print "鍛冶屋シナガー「ヒロウ村は坂を降りたところだよ」")
-   (show-trigger 'ui:smith-3->hilou "ヒロウ村へ"))
- loc:smith-3)
+   (ft-show-trigger 'ui:smith-3->hilou "ヒロウ村へ"))
+ scn:smith-3)
 
-#;(define pick-knife
- (mecs-new-func (lambda (ui:pick-knife) ; -> loc:smith-3
-                  (skip-if (not ui:pick-knife))
-                  (print "シナガーの包丁を手に入れた")
-                  #t)))
+(ft-add-path! ui:pick-knife scn:smith-2 scn:smith-3)
 
-(add-path! ui:pick-knife loc:smith-2 loc:smith-3)
-
-(add-location!
- "山"
+(ft-add-scene!
+ "オシャーゲ山 1"
  (lambda ()
    (print "わたし「あそこに光る石がある！」")
-   (print "わたし「あれを鍛冶屋さんに持って行けばいいのね」")
-   (show-trigger 'ui:pick-stone "石を拾う")
+   (ft-show-trigger 'ui:pick-stone "石を拾う")
    )
- loc:hill)
+ scn:hill-1)
 
-(add-path! ui:smith-1->hill loc:smith-1 loc:hill)
-(add-path! ui:hill->smith-2 loc:hill loc:smith-2)
+(ft-add-scene!
+ "オシャーゲ山 2"
+ (lambda ()
+   (print "わたし「これを鍛冶屋さんに持って行けばいいのね」")
+   (ft-show-trigger 'ui:hill-2->smith-2 "鍛冶屋さんへ")
+   )
+ scn:hill-2)
 
-#;(define pick-stone
- (mecs-new-func (lambda (ui:pick-stone) ; -> has-stone?
-                  (skip-if (not ui:pick-stone))
-                  (print "光る石を拾った")
-                  #t)))
+(ft-add-path! ui:pick-stone scn:hill-1 scn:hill-2)
 
-#;(mecs-connect! pick-stone `(,ui:pick-stone) `(,has-stone?))
+(ft-add-path! ui:smith-1->hill-1 scn:smith-1 scn:hill-1)
+(ft-add-path! ui:hill-2->smith-2 scn:hill-2 scn:smith-2)
 
 ;; ヒロウ村へ
-;; 一方通行
-(add-path! ui:smith-3->hilou loc:smith-3 loc:hilou)
+(ft-add-path! ui:smith-3->hilou scn:smith-3 scn:hilou)
 
-(add-location!
+(ft-add-scene!
  "ヒロウ村"
  (lambda ()
    (print "わたし「おじちゃんはどこかな？」")
    (print "村人「おじいちゃんは港の近くの家にいるよ」")
    )
- loc:hilou)
+ scn:hilou)
--- game/test.scm
+++ game/test.scm
@@ -5,17 +5,23 @@
 
 (test-section "初めの町")
 
-(mecs-update! `((,loc:home . #t)))
+(mecs-update! `((,scn:home . #t)))
 
 (mecs-update! `((,ui:home->smith-1 . #t)))
 
-;(mecs-update! `((,has-knife? . #f) (,has-stone? . #f)))
+(test* "smith-1" #t (mecs-var-value (mecs-var-node-var scn:smith-1)))
 
-(mecs-update! `((,ui:smith-1->hill . #t)))
+(mecs-update! `((,ui:smith-1->hill-1 . #t)))
+
+(test* "hill-1" #t (mecs-var-value (mecs-var-node-var scn:hill-1)))
 
 (mecs-update! `((,ui:pick-stone . #t)))
 
-(mecs-update! `((,ui:hill->smith-2 . #t)))
+(test* "hill-2" #t (mecs-var-value (mecs-var-node-var scn:hill-2)))
+
+(mecs-update! `((,ui:hill-2->smith-2 . #t)))
+
+(test* "smith-2" #t (mecs-var-value (mecs-var-node-var scn:smith-2)))
 
 (mecs-update! `((,ui:pick-knife . #t)))
 
